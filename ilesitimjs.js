6181ec4d6a47f1a7d5b5270cace8586ed05460cd
Ömer Varol <62489749+daw1d1@users.noreply.github.com>
2020-06-19 10:21:18 +0300

    sad

---
+++ ilesitimjs.js
@@ -0,0 +1,7 @@
+<script>
+"use strict";
+var formxd=document.forms["formxd"];
+var eposta=formxd.eposta;
+alert(eposta)
+
+</script>
---
+++ myscript.js
@@ -0,0 +1,9 @@
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
