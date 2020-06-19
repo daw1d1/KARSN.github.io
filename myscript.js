c5ecf849017ae6f17ee74f59f2f451f312f4e9bf
Ömer Varol <62489749+daw1d1@users.noreply.github.com>
2020-06-19 11:47:31 +0300

    asd

--- myscript.js
+++ myscript.js
@@ -8,38 +8,3 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-
-<script>
-"use strict";
-  var _slayt = document.getElementsByClassName("slayt");
-  var slaytSayisi = _slayt.length;
-  var slaytNo = 0;
-  var i = 0;
-
-  slaytGoster(slaytNo);
-
-  function sonrakiSlayt() {
-    slaytNo++;
-    slaytGoster(slaytNo);
-  }
-
-  function oncekiSlayt() {
-    slaytNo--;
-    slaytGoster(slaytNo);
-  }
-
-  function slaytGoster(slaytNumarasi) {
-    slaytNo = slaytNumarasi;
-
-    if (slaytNumarasi >= slaytSayisi) slaytNo = 0;
-
-    if (slaytNumarasi < 0) slaytNo = slaytSayisi - 1;
-
-    for (i = 0; i < slaytSayisi; i++) {
-      _slayt[i].style.display = "none";
-    }
-
-    _slayt[slaytNo].style.display = "block";
-
-  }
-</script>
